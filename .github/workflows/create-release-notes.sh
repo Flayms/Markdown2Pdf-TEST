0c671ac8628210b5ae0e0a5aaa7cbe91569571a4
Anton Lange <der.anton111@gmail.com>
2023-06-22 22:37:45 +0200

    Update create-release-notes.sh

--- .github/workflows/create-release-notes.sh
+++ .github/workflows/create-release-notes.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-FILE_NAME=release-notes.md
-echo "**Full Changelog**: https://github.com/$OWNER_AND_REPO_NAME/compare" >> $FILE_NAME
+
+OWNER_AND_REPO_NAME=$1
+CURRENT_TAG=$2
+FILE_NAME=$3
+
+LAST_TAG=$(git describe --tags --abbrev=0)
+
+echo "Creating $FILE_NAME"
+
+git log $LAST_TAG..HEAD --no-merges --oneline > $FILE_NAME
+echo "" >> $FILE_NAME
+echo "---" >> $FILE_NAME
+echo "**Full Changelog**: https://github.com/$OWNER_AND_REPO_NAME/compare/$LAST_TAG...$CURRENT_TAG" >> $FILE_NAME

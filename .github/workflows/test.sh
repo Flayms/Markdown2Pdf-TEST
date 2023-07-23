83223d821de9c5a254be2083f70aca58dbf06a34
Anton Lange <der.anton111@gmail.com>
2023-07-23 21:42:43 +0200

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -2,6 +2,6 @@
 
 TEST_VALUE="Hello Variable!"
 echo "MyTestVar=$TEST_VALUE" >> $GITHUB_ENV
-echo "MyTestVar2="Hello World2!" >> $GITHUB_ENV
+echo "MyTestVar2=Hello World2!" >> $GITHUB_ENV
 
 #echo "Retreived Value ${{ env.MyTestVar }}"
